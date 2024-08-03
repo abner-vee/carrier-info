05023886c3cbfd9ccb869e19f2c2f58f9c422eb0
Vivian Abner <abnervivian@gmail.com>
2024-08-03 12:37:35 +0100

    remove json file

--- src/component/Table.jsx
+++ src/component/Table.jsx
@@ -25,7 +25,7 @@ export const DataGridDemo = () => {
     React.useEffect(() => {
         const fetchData = async () => {
             try {
-                const response = await fetch("http://localhost:5000", {
+                const response = await fetch("https://carrier-info-backend.onrender.com", {
                     method: 'GET',
                     headers: {
                         'Content-Type': 'application/json',
